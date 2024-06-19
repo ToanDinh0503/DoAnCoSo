0e99e4340eee4adfa81c122905d906f5b6822eaf
lebao0112 <lebao0112>
2024-06-19 19:41:54 +0700

    làm lại chatbot

--- WebBG/obj/Debug/net8.0/WebBG.AssemblyInfo.cs
+++ WebBG/obj/Debug/net8.0/WebBG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c20f0c10ffb57e9e4bca3247f10b545f5eddcca")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
