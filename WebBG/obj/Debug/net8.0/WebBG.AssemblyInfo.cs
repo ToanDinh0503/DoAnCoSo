ac53f99e52b5b1b21485d82516f07652116d16bb
lebao0112 <lebao0112>
2024-06-20 09:59:39 +0700

    thêm chức năng random mã khuyến mãi

--- WebBG/obj/Debug/net8.0/WebBG.AssemblyInfo.cs
+++ WebBG/obj/Debug/net8.0/WebBG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c20f0c10ffb57e9e4bca3247f10b545f5eddcca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e99e4340eee4adfa81c122905d906f5b6822eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
